ed005a55bb9a38e820ffe6610dd5703742c39fd3
ChangZhehao <changzhehao@gmail.com>
2019-02-28 10:41:59 +1100

    first upload

---
+++ src/main/java/EnumExceptionType.java
@@ -0,0 +1,14 @@
+/**
+ * User: u6613739
+ * Date: 2019/2/28
+ * Time: 10:13
+ * Description:
+ */
+public enum EnumExceptionType
+{
+    SUCCESS,
+    ERROR,
+    WARNNING,
+    FATAL,
+    UNKOWN
+}
